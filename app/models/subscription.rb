59c6b8da32813c53404b421cf56de768de864f49
= <dheeru.manthani@gmail.com>
2015-09-22 23:34:37 +0530

    updated notification

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -23,7 +23,7 @@ class Subscription < ActiveRecord::Base
         user.stripe_charges.create(amount: stripe_charge_amount(user), currency: "usd", description: "test", stripe_customer_id: stripe_customer.id)
       elsif user.plan_name == "football"
         stripe_charge = Stripe::Charge.create(amount: stripe_football_amount(user), currency: "usd", customer: s_customer.id, description: "Charge for test@example.com")
-        user.stripe_charges.create(amount: stripe_charge_amount(user), currency: "usd", description: "test", stripe_customer_id: stripe_customer.id)
+        user.stripe_charges.create(amount: stripe_football_amount(user), currency: "usd", description: "test", stripe_customer_id: stripe_customer.id)
       else
         subscription = customer.subscriptions.create(:plan => payment_discount)
         user.subscriptions.create(stripe_card_token: subscription.id,plan_name: user.plan_name, stripe_customer_id: stripe_customer.id)
@@ -72,11 +72,11 @@ class Subscription < ActiveRecord::Base
   end
 
   def stripe_charge_amount(user)
-    (user.last.holiday.reject { |c| c.empty? }.count) * 25 * 100
+    (user.holiday.reject { |c| c.empty? }.count) * 25 * 100
   end
   
   def stripe_football_amount(user)
-    (user.amount) * 100
+    (user.amount.to_i) * 100
   end
 
   # def check_subscription(user)
