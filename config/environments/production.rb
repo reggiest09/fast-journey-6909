13f5f0136d722b5bd46544d3fdb34a1b052b8b40
nagendra <nagendrababu0701@gmail.com>
2016-03-17 03:00:18 +0530

    added production changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,8 +45,9 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :debug
-
+  #config.log_level = :debug
+  config.logger = Logger.new(STDOUT) 
+  config.log_level = :info
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
 
@@ -75,6 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: 'intense-tor-91881.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   # # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
