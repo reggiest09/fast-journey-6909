31ac4df6cd9b3c5eacb3f3170f84d58e8b4771d2
Reggie <reggiest09@gmail.com>
2016-07-26 12:30:19 -0400

    stp

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,4 @@
-# Stripe.api_key =  ENV["STRIPE_API_KEY"] #'sk_test_dqHtH82qUFlMUt3ZTbja6nAJ'
-# STRIPE_PUBLIC_KEY = ENV["STRIPE_PUBLIC_KEY"] #'pk_test_lOnLX79prFRRQgIUNzUmIdzR'
+ Stripe.api_key =  ENV["STRIPE_API_KEY"] #'sk_test_dqHtH82qUFlMUt3ZTbja6nAJ'
+ STRIPE_PUBLIC_KEY = ENV["STRIPE_PUBLIC_KEY"] #'pk_test_lOnLX79prFRRQgIUNzUmIdzR'
 
 
-Stripe.api_key = 'sk_test_dqHtH82qUFlMUt3ZTbja6nAJ'
-STRIPE_PUBLIC_KEY = 'pk_test_lOnLX79prFRRQgIUNzUmIdzR'
