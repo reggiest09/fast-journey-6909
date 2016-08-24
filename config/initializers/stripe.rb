465804c6fc5e4a0dd3c41521415abf9450c56b30
prashantsunoray <prashant.chaudhari@sunoray.com>
2016-08-24 19:55:24 +0530

    Donate link and stripe crdentials

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,2 @@
- Stripe.api_key =  'sk_test_nUagza6fEuvTLURMBK4eIEMn'
- STRIPE_PUBLIC_KEY = 'pk_test_zLYzIdRSvwjya37HpuH76u4t'
+Stripe.api_key =  ENV["STRIPE_API_KEY"] 
+STRIPE_PUBLIC_KEY = ENV["STRIPE_PUBLIC_KEY"]
