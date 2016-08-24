4f2bf0ce1c9ac9ee5ee9c5367ce077623663c86a
prashantsunoray <prashant.chaudhari@sunoray.com>
2016-08-24 19:37:28 +0530

    Added assets path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,8 @@ Rails.application.configure do
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
 
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+  config.assets.precompile += %w( .svg .eot .woff .ttf)
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
