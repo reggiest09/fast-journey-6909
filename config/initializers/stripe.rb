5b6b0f8d651e69402c68e5922ba787ac9a387278
Reggie <reggiest09@gmail.com>
2016-11-01 22:26:55 -0400

    bball fix

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,4 @@
-Stripe.api_key =  ENV["STRIPE_API_KEY"] 
- STRIPE_PUBLIC_KEY = ENV["STRIPE_PUBLIC_KEY"] 
+ Stripe.api_key =  ENV["STRIPE_API_KEY"] 
+  STRIPE_PUBLIC_KEY = ENV["STRIPE_PUBLIC_KEY"] 
+#Stripe.api_key =  "sk_test_nUagza6fEuvTLURMBK4eIEMn"
+ #STRIPE_PUBLIC_KEY = "pk_test_zLYzIdRSvwjya37HpuH76u4t"
